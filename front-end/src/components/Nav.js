239a58d86fb1546740e972b495e3863ede291b9f
SatishSingh0313 <satishsangam.205@gmail.com>
2024-04-23 20:59:24 +0530

    E-Commerce ( Using Mern )

--- front-end/src/components/Nav.js
+++ front-end/src/components/Nav.js
@@ -18,6 +18,7 @@ const Nav = () => {
                 <li><Link to="/add">Add Product</Link></li>
                 <li><Link to="/list">Update Products</Link></li>
                 <li><Link to="/profile">Profile </Link></li>
+                <li><Link to="/about">About</Link></li>
                 <li ><Link onClick={logout} to="/signup">Logout ({JSON.parse(auth).name})</Link></li>
             </ul>
                 :
