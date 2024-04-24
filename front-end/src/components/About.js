84b65dc2199a386e754d1a8a4fabd4f50b602218
SatishSingh0313 <satishsangam.205@gmail.com>
2024-04-24 15:26:58 +0530

    E-Commerce ( Using Mern )

--- front-end/src/components/About.js
+++ front-end/src/components/About.js
@@ -1,13 +1,19 @@
+import logo from "../logo.jpg"
 
 const About = () => {
 
 
-    return(
-        <div  className="login">
-            <h1>About Page</h1>
+    return (
+        <div className="login">
+            <img src={logo} alt=""></img>
+            <h2>You Think We Built Your Vision</h2>
+            <h1>We Mind Your Business !!</h1>
+            <p><h3>Are you desiring to achieve your digital dream with the best-combined services?
+                Astream is here for you, to provide all types of digital support regarding personal or professional as well as e-commercial website issues. Apart from this,
+                we deliver a wide range of services that meet your specific needs.</h3></p>
         </div>
     )
-      
+
 }
 
 export default About;
