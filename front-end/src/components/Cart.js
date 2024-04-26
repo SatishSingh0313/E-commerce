2bcaf2114b65b8ef18ca6c693b330d1bef4b5602
SatishSingh0313 <satishsangam.205@gmail.com>
2024-04-26 22:38:02 +0530

    E-Commerce ( Using Mern )

--- front-end/src/components/Cart.js
+++ front-end/src/components/Cart.js
@@ -1,11 +1,37 @@
+import React, { useState } from "react";
 
-const Cart =()=>{
+const Cart = () => {
+  const [cartItems, setCartItems] = useState([]);
 
-    return(
-        <div  className="login">
-            <h1>This is a Cart Componenet</h1>
-        </div>
+  const addToCart = (product) => {
+    setCartItems([...cartItems, product]);
+  };
 
-    )
-}
+  const removeFromCart = (index) => {
+    const updatedCartItems = [...cartItems];
+    updatedCartItems.splice(index, 1);
+    setCartItems(updatedCartItems);
+  };
+
+  const totalPrice = cartItems.reduce((acc, item) => acc + item.price, 0);
+
+  return (
+    <div className="cart">
+      <h2>Shopping Cart</h2>
+      <ul>
+        {cartItems.map((item, index) => (
+          <li key={index}>
+            {item.name} - ${item.price}
+            <button onClick={() => removeFromCart(index)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+      <p>Total Price: ${totalPrice}</p>
+      <button onClick={() => addToCart({ name: 'Product', price: 10 })}>
+        Add Product
+      </button>
+    </div>
+  );
+};
+
 export default Cart;
