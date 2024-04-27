a5267d79cb976a7eb2c7e51935a4c4f726bf7aea
SatishSingh0313 <satishsangam.205@gmail.com>
2024-04-27 11:47:50 +0530

    E-Commerce ( Using Mern )

--- front-end/src/components/Cart.js
+++ front-end/src/components/Cart.js
@@ -1,35 +1,29 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
 const Cart = () => {
   const [cartItems, setCartItems] = useState([]);
 
-  const addToCart = (product) => {
-    setCartItems([...cartItems, product]);
+  const addItemToCart = (item) => {
+    setCartItems([...cartItems, item]);
   };
 
-  const removeFromCart = (index) => {
-    const updatedCartItems = [...cartItems];
-    updatedCartItems.splice(index, 1);
-    setCartItems(updatedCartItems);
+  const removeItemFromCart = (index) => {
+    const newCartItems = [...cartItems];
+    newCartItems.splice(index, 1);
+    setCartItems(newCartItems);
   };
-
-  const totalPrice = cartItems.reduce((acc, item) => acc + item.price, 0);
 
   return (
-    <div className="cart">
+    <div>
       <h2>Shopping Cart</h2>
       <ul>
         {cartItems.map((item, index) => (
           <li key={index}>
-            {item.name} - ${item.price}
-            <button onClick={() => removeFromCart(index)}>Remove</button>
+            {item.name} - ${item.price}{' '}
+            <button onClick={() => removeItemFromCart(index)}>Remove</button>
           </li>
         ))}
       </ul>
-      <p>Total Price: ${totalPrice}</p>
-      <button onClick={() => addToCart({ name: 'Product', price: 10 })}>
-        Add Product
-      </button>
     </div>
   );
 };
