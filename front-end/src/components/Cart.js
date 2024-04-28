733a59e483dc08ac66df96cec6791bf31cdbe233
SatishSingh0313 <satishsangam.205@gmail.com>
2024-04-28 13:44:56 +0530

    E-Commerce ( Using Mern )

--- front-end/src/components/Cart.js
+++ front-end/src/components/Cart.js
@@ -1,35 +1,10 @@
-import React, { useState } from "react";
 
 const Cart = () => {
-  const [cartItems, setCartItems] = useState([]);
-
-  const addToCart = (product) => {
-    setCartItems([...cartItems, product]);
-  };
-
-  const removeFromCart = (index) => {
-    const updatedCartItems = [...cartItems];
-    updatedCartItems.splice(index, 1);
-    setCartItems(updatedCartItems);
-  };
-
-  const totalPrice = cartItems.reduce((acc, item) => acc + item.price, 0);
-
+  
   return (
-    <div className="cart">
+    <div className="login">
       <h2>Shopping Cart</h2>
-      <ul>
-        {cartItems.map((item, index) => (
-          <li key={index}>
-            {item.name} - ${item.price}
-            <button onClick={() => removeFromCart(index)}>Remove</button>
-          </li>
-        ))}
-      </ul>
-      <p>Total Price: ${totalPrice}</p>
-      <button onClick={() => addToCart({ name: 'Product', price: 10 })}>
-        Add Product
-      </button>
+      
     </div>
   );
 };
