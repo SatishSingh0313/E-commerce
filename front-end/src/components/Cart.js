666cca4128fa1a4eeb1147a4aa6045ad4855264f
SatishSingh0313 <satishsangam.205@gmail.com>
2024-05-01 20:51:06 +0530

    E-Commerce ( Using Mern )

--- front-end/src/components/Cart.js
+++ front-end/src/components/Cart.js
@@ -4,12 +4,13 @@ const Cart = () => {
    const addToCard = ()=>{
         let item=localStorage.getItem("user");
         console.log(item);
-        
+
    }
   return (
     <div className="login">
       <h2>Shopping Cart</h2>
-       <button onClick={addToCard}>Card</button>
+      <h1>Your All Item </h1>
+       <button onClick={addToCard}>Click</button>
     </div>
   );
 };
